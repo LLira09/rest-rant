e9a86ea8949058f7ccabf780375d4dfde45d31ad
Jake Lira <llira09@gmail.com>
2022-12-13 17:24:11 -0600

    Adding default view for layout

---
+++ views/default.js
@@ -0,0 +1,16 @@
+const Rect = require('react')
+
+function Def(html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = Def
